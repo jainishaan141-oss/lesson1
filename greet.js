89bbc67b3c46340a6bc64e1eb96a2f8f9790edfb
jainishaan141-oss <jainishaan141@gmail.com>
2026-05-30 19:48:29 +0530

    Add files via upload

---
+++ greet.js
@@ -0,0 +1,7 @@
+function greet(name){
+    console.log("hello", name)
+}
+
+greet("ishaan")
+greet("suresh")
+greet("bob")
---
+++ scope.js
@@ -0,0 +1,13 @@
+let school= "BookLY school"
+function showschool(){
+    console.log(school)
+}
+showschool()
+
+let name ="alice"
+function studentinfo(){
+    let name ="india"
+    console.log(name)
+}
+studentinfo()
+console.log(name)
