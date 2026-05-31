92fb6e3cf500135eca37d1a669b64d7d229bab5d
jainishaan141-oss <jainishaan141@gmail.com>
2026-05-31 10:14:33 +0530

    Add files via upload

---
+++ expression.js
@@ -0,0 +1,9 @@
+let square = function(x){
+    console.log(x * x)
+    console.log(x + x)
+    console.log(x / x)
+    console.log(x - x)
+    console.log(x % x)
+    console.log(x ** x)
+}
+square(5)
