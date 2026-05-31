9a3b677dcc2a7ea6160bfc99c5900c2c3a1a20b2
jainishaan141-oss <jainishaan141@gmail.com>
2026-05-31 21:53:06 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,24 @@
+let a = 5
+let b = 10
+let c = 15
+let d = 20
+let e = 25
+
+let age = 55
+let mark = 110
+let number = 220
+
+console.log(a + b)
+console.log(b - c)
+console.log(c * d)
+console.log(d % e)
+console.log(e / a)
+console.log(a ** b)
+
+console.log(age + mark)
+console.log(mark - number)
+console.log(number * age)
+console.log(age % mark)
+console.log(mark / number)
+console.log(number ** age)
+
