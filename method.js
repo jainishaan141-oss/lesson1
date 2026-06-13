adee43fd9345fb4eea3127493467ddd5c90d2b3a
jainishaan141-oss <jainishaan141@gmail.com>
2026-06-13 19:40:19 +0530

    Add files via upload

---
+++ method.js
@@ -0,0 +1,17 @@
+let arr = ["apple", "mango", "banana", "grapes"]
+
+arr[1] = "orange"
+console.log(arr)
+
+console.log(arr.length)
+
+arr.push = "watermelon"
+console.log(arr)
+
+arr.pop ()
+console.log(arr)
+arr.shift ()
+console.log(arr)
+arr.unshift("apple")
+console.log(arr)
+
